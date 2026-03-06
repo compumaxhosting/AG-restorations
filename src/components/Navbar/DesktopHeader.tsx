0fc1f22f489fd251de3df5bc95bc2eeac25bd1a7
compumaxhosting <compumaxhost@gmail.com>
2026-03-06 19:09:03 +0530

    ff

--- src/components/Navbar/DesktopHeader.tsx
+++ src/components/Navbar/DesktopHeader.tsx
@@ -5,22 +5,18 @@ import NavLinks from "./NavLinks";
 import TopBar from "./TopBar";
 import Link from "next/link";
 
-export default function DesktopHeader({
-  setSidebarOpen,
-}: {
-  setSidebarOpen: (val: boolean) => void;
-}) {
+export default function DesktopHeader() {
   return (
     <header className="relative z-50 hidden md:block">
       {/* Logo Section with clip-path */}
       <Link
         href="/"
         className="absolute left-0 top-0 h-full w-102.5 bg-[#e63a27] z-10 flex items-center justify-center [clip-path:polygon(0_0,100%_0,84%_100%,0%_100%)]"
-        aria-label="Home link with company logo" // Added for screen readers
+        aria-label="Home link with company logo"
       >
         <Image
           src="/Navbar/Logo-AG.png"
-          alt="Company Logo" // Already good!
+          alt="Company Logo"
           width={260}
           height={120}
           className="object-contain cursor-pointer"
@@ -32,9 +28,9 @@ export default function DesktopHeader({
 
       <nav
         className="flex justify-end items-center gap-5 px-6 bg-white shadow-md h-20"
-        aria-label="Main navigation" // Added for screen readers
+        aria-label="Main navigation"
       >
-        <NavLinks setSidebarOpen={setSidebarOpen} />
+        <NavLinks />
       </nav>
     </header>
   );
