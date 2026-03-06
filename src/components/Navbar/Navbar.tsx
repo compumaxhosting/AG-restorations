064f168b3411141c6048d83e20887eb42ef15820
compumaxhosting <compumaxhost@gmail.com>
2026-03-06 19:13:36 +0530

    cfd

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,15 +5,13 @@ import DesktopHeader from "./DesktopHeader";
 import SidebarOverlay from "./SidebarOverlay";
 import MobileHeaderNew from "./MobileHeaderNew";
 
-
 export default function Navbar() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-  
 
   return (
     <>
       <MobileHeaderNew />
-      <DesktopHeader setSidebarOpen={setSidebarOpen} />
+      <DesktopHeader />
       {sidebarOpen && <SidebarOverlay onClose={() => setSidebarOpen(false)} />}
     </>
   );
