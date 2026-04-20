996a04ad8b41986e6c4cafb929b0b9e5033fdf6f
compumaxhosting <compumaxhost@gmail.com>
2026-04-20 19:43:20 +0530

    site map updated with correct priorities.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,33 +10,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
   }[] = [
     {
       path: "",
-      priority: 1,
+      priority: 1.0,
       changeFrequency: "weekly",
     },
-    {
-      path: "/aboutus",
-      priority: 0.8,
-      changeFrequency: "monthly",
-    },
+
     {
       path: "/services",
       priority: 0.9,
       changeFrequency: "monthly",
-    },
-    {
-      path: "/projects",
-      priority: 0.8,
-      changeFrequency: "monthly",
-    },
-    {
-      path: "/blog",
-      priority: 0.9,
-      changeFrequency: "weekly",
-    },
-    {
-      path: "/contact-us",
-      priority: 0.7,
-      changeFrequency: "yearly",
     },
     {
       path: "/roofing-services-linden-nj",
@@ -53,11 +34,40 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
       changeFrequency: "monthly",
     },
+
+    {
+      path: "/aboutus",
+      priority: 0.7,
+      changeFrequency: "monthly",
+    },
+    {
+      path: "/projects",
+      priority: 0.7,
+      changeFrequency: "monthly",
+    },
+
+    {
+      path: "/contact-us",
+      priority: 0.6,
+      changeFrequency: "yearly",
+    },
+
+    {
+      path: "/blog",
+      priority: 0.6,
+      changeFrequency: "weekly",
+    },
+
+    {
+      path: "/blog/best-roofing-contractor-linden-nj",
+      priority: 0.4,
+      changeFrequency: "monthly",
+    },
   ];
 
   return routes.map((route) => ({
     url: `${baseUrl}${route.path}`,
-    lastModified: new Date(),
+    lastModified: new Date(), // 🔥 you can later replace with real dates
     changeFrequency: route.changeFrequency,
     priority: route.priority,
   }));
